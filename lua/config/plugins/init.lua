7cc4fb50c6b4df949e15cfbe79c29fed296cd5af
Paul Zupan <paul.zupan.13@gmail.com>
2023-04-10 11:54:40 -0700

    Add treesitter support

--- lua/config/plugins/init.lua
+++ lua/config/plugins/init.lua
@@ -1,5 +1,14 @@
 local plugins = {
   {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("config.plugins.treesitter")
+    end,
+    run = function()
+      pcall(vim.cmd.TSUpdate)
+    end,
+  },
+  {
     "sainnhe/gruvbox-material",
     config = function()
       vim.cmd.colorscheme("gruvbox-material")
