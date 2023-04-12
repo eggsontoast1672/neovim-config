d788ea0d6b15e7918b6b877e0478d0d87f167344
Paul Zupan <paul.zupan.13@gmail.com>
2023-04-11 20:04:30 -0700

    Add a few treesitter languages

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,5 +1,5 @@
 require("nvim-treesitter.configs").setup({
-    ensure_installed = { "cmake", "cpp", "lua", "markdown" },
+    ensure_installed = { "cmake", "cpp", "css", "html", "lua", "markdown", "vim" },
     highlight = {
         enable = true,
     },
